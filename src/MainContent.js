1b4a8852139e726b597874b3261fa8be72f33410
Beto <betomoedano@outlook.com>
2021-05-25 07:10:37 -0400

    jit tailwind added

--- src/MainContent.js
+++ src/MainContent.js
@@ -6,7 +6,13 @@ function MainContent() {
         <div>
             <h1 className="text-7xl pb-10 pt-20">Guess Crypto</h1>
             <p className="pb-10">Crypto Guess is a revolutionary way to earn crypto by guessing, yeah! literally guessing</p>
-            <iframe src='https://my.spline.design/coin-f76d5609439b3981bbabbfd69318f31d/' frameborder='0' width='100%' height='100%'></iframe>
+            <iframe 
+                title="coin" 
+                src='https://my.spline.design/coin-f76d5609439b3981bbabbfd69318f31d/' 
+                frameborder='0' 
+                width='100%' 
+                height='100%'
+            ></iframe>
             <Btn 
                 name="Buy Now"
                 link="https://metamask.io/"
